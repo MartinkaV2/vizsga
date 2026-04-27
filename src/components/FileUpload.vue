c143fb53e721efeec6240a3f46e8b3ee87483aaf
MartinkaV2 <medveczmartin@gmail.com>
2026-04-27 12:22:27 +0200

    kisebb

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card p-4">
     <h3>Vizsga feltöltő oldal</h3>
-    <h4>Készítette: Medvecz Martin</h4>
+    <h6>Készítette: Medvecz Martin</h6>
 
     <input type="file" @change="handleFile" class="form-control mb-3" />
 
